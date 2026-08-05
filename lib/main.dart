e9585f61a57de95e3caff1dbd0f66027effc77a0
Wazhma Hakimi <133529903+WazhmaHakimi@users.noreply.github.com>
2026-08-05 18:48:56 +0430

    Separating App Initialization from Login and Register Screens

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:my_notes/firebase_options.dart';
 import 'package:my_notes/views/login_view.dart';
 
@@ -12,43 +12,19 @@ void main() {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
-      home: const LoginView(),
+      home: const HomePage(),
     ),
   );
 }
 
-class RegisterView extends StatefulWidget {
-  const RegisterView({super.key});
-
-  @override
-  State<RegisterView> createState() => _RegisterViewState();
-}
-
-class _RegisterViewState extends State<RegisterView> {
-  late final TextEditingController _email;
-  late final TextEditingController _password;
-
-  @override
-  void initState() {
-    _email = TextEditingController();
-    _password = TextEditingController();
-
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    _email.dispose();
-    _password.dispose();
-
-    super.dispose();
-  }
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Register', style: TextStyle(color: Colors.white)),
+        title: const Text('Home Page', style: TextStyle(color: Colors.white)),
         backgroundColor: Colors.black,
       ),
 
@@ -56,53 +32,19 @@ class _RegisterViewState extends State<RegisterView> {
         future: Firebase.initializeApp(
           options: DefaultFirebaseOptions.currentPlatform,
         ),
-        builder: (context, asyncSnapshot) {
-          return Column(
-            children: [
-              TextField(
-                controller: _email,
-                enableSuggestions: false,
-                autocorrect: false,
-                keyboardType: TextInputType.emailAddress,
-                decoration: InputDecoration(
-                  hintText: 'Please enter your email',
-                ),
-              ),
-              TextField(
-                controller: _password,
-                obscureText: true,
-                enableSuggestions: false,
-                autocorrect: false,
-                decoration: InputDecoration(
-                  hintText: 'Please enter your password',
-                ),
-              ),
-              TextButton(
-                onPressed: () async {
-                  final email = _email.text;
-                  final password = _password.text;
-                  try {
-                    final userCredential = await FirebaseAuth.instance
-                        .createUserWithEmailAndPassword(
-                          email: email,
-                          password: password,
-                        );
-
-                    print(userCredential);
-                  } on FirebaseAuthException catch (e) {
-                    if (e.code == 'weak-password') {
-                      print('Weak Password');
-                    } else if (e.code == 'email-already-in-use') {
-                      print('Email is already in used.');
-                    } else if (e.code == 'invalid-email') {
-                      print('Invalid Email');
-                    }
-                  }
-                },
-                child: Text('Register'),
-              ),
-            ],
-          );
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.done:
+              final user = FirebaseAuth.instance.currentUser;
+              if (user?.emailVerified ?? false) {
+                print('You are a verified user');
+              } else {
+                print('You need to verify your email first.');
+              }
+              return const Text('Done');
+            default:
+              return const Text('Loading...');
+          }
         },
       ),
     );
