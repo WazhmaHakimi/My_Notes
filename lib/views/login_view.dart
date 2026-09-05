b6a147ee1d6a968d1ab6dfaf05cc40579bc4b528
Wazhma Hakimi <133529903+WazhmaHakimi@users.noreply.github.com>
2026-09-05 17:18:30 +0430

    User that isn't verified is redirected to teh verify your email page from login.

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -77,9 +77,18 @@ class _LoginViewState extends State<LoginView> {
                           password: password,
                         );
 
-                    Navigator.of(
-                      context,
-                    ).pushNamedAndRemoveUntil(notesRoute, (route) => false);
+                    final user = FirebaseAuth.instance.currentUser;
+
+                    if (user?.emailVerified ?? false) {
+                      Navigator.of(
+                        context,
+                      ).pushNamedAndRemoveUntil(notesRoute, (route) => false);
+                    } else {
+                      Navigator.of(context).pushNamedAndRemoveUntil(
+                        verifyEmailRoute,
+                        (route) => false,
+                      );
+                    }
                   } on FirebaseAuthException catch (e) {
                     log(e.code.toString());
                     if (e.code == 'invalid-credential') {
